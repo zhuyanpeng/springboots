38a28ea9ed8a67db6883cf1fcea548d278659187
看海的人 <zhuyanpengyy@163.com>
2018-02-02 09:39:58 +0800

    springboot动态修改logger的级别

---
+++ demo28-logger/src/test/java/com/cnnnc/www/Demo28LoggerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cnnnc.www;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Demo28LoggerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
