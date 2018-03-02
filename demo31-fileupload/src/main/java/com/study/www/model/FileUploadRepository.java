2f9671c9034c1534af26a9e0583908f5d9f6bd98
看海的人 <zhuyanpengyy@163.com>
2018-03-02 17:06:22 +0800

    springboot 文件的上传和下载

---
+++ demo31-fileupload/src/main/java/com/study/www/model/FileUploadRepository.java
@@ -0,0 +1,13 @@
+package com.study.www.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * FileUploadRepository:
+ *
+ * @auther : Administrator.zhuyanpeng
+ * @date :  2018/3/1    16:56
+ **/
+public interface FileUploadRepository extends JpaRepository<FileUpload,Long>{
+
+}
